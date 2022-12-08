b899cb41c346c29868ef5be6ea1e74b479cad924
wangmao <wangcaowu@gmail.com>
2022-12-08 18:33:24 +0800

    feat: add resnet of classify

--- sources/libs/nvdsinfer_customparser/nvdsinfer_customclassifierparser.cpp
+++ sources/libs/nvdsinfer_customparser/nvdsinfer_customclassifierparser.cpp
@@ -99,5 +99,79 @@ bool NvDsInferClassiferParseCustomSoftmax (std::vector<NvDsInferLayerInfo> const
     return true;
 }
 
+extern "C"
+bool NvDsInferClassiferParseSoftmaxGetmax (std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
+        NvDsInferNetworkInfo  const &networkInfo,
+        float classifierThreshold,
+        std::vector<NvDsInferAttribute> &attrList,
+        std::string &descString);
+
+static std::vector < std::vector< std:: string > > car_head_tail_labels { {
+    "head", "tail", "unknow"} };
+
+extern "C"
+bool NvDsInferClassiferParseSoftmaxGetmax (std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
+        NvDsInferNetworkInfo  const &networkInfo,
+        float classifierThreshold,
+        std::vector<NvDsInferAttribute> &attrList,
+        std::string &descString)
+{
+    /* Get the number of attributes supported by the classifier. */
+    unsigned int numAttributes = outputLayersInfo.size();
+
+    /* Iterate through all the output coverage layers of the classifier.
+    */
+    for (unsigned int l = 0; l < numAttributes; l++)
+    {
+        /* outputCoverageBuffer for classifiers is usually a softmax layer.
+         * The layer is an array of probabilities of the object belonging
+         * to each class with each probability being in the range [0,1] and
+         * sum all probabilities will be 1.
+         */
+        NvDsInferDimsCHW dims;
+
+        getDimsCHWFromDims(dims, outputLayersInfo[l].inferDims);
+        unsigned int numClasses = dims.c;
+        float *outputCoverageBuffer = (float *)outputLayersInfo[l].buffer;
+        float maxProbability = 0;
+        bool attrFound = false;
+        NvDsInferAttribute attr;
+
+        /* Iterate through all the probabilities that the object belongs to
+         * each class. Find the maximum probability and the corresponding class
+         * which meets the minimum threshold. */
+        for (unsigned int c = 0; c < numClasses; c++)
+        {
+            float probability = outputCoverageBuffer[c];
+            // printf("---- %f \n", probability);
+            // if (probability > classifierThreshold
+            if (c == 0 ||
+                probability > maxProbability)
+            {
+                maxProbability = probability;
+                attrFound = true;
+                attr.attributeIndex = l;
+                attr.attributeValue = c;
+                attr.attributeConfidence = probability;
+            }
+        }
+        if (attrFound)
+        {
+            if (car_head_tail_labels.size() > attr.attributeIndex &&
+                    attr.attributeValue < car_head_tail_labels[attr.attributeIndex].size())
+                attr.attributeLabel =
+                    strdup(car_head_tail_labels[attr.attributeIndex][attr.attributeValue].c_str());
+            else
+                attr.attributeLabel = nullptr;
+            attrList.push_back(attr);
+            if (attr.attributeLabel)
+                descString.append(attr.attributeLabel).append(" ");
+        }
+    }
+
+    return true;
+}
+
 /* Check that the custom function has been defined correctly */
 CHECK_CUSTOM_CLASSIFIER_PARSE_FUNC_PROTOTYPE(NvDsInferClassiferParseCustomSoftmax);
+CHECK_CUSTOM_CLASSIFIER_PARSE_FUNC_PROTOTYPE(NvDsInferClassiferParseSoftmaxGetmax);
